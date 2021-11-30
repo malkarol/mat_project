ee98135b2f64448ea0df291307cca3d9523f8464
lemiecm <mjlemiec@gmail.com>
2021-11-30 20:54:31 +0100

    Update Login.vue

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,25 +1,31 @@
 <template>
-  <div class="row my-5 col-md-12 justify-content-center">
-    <div class="col-md-2 justify-content-center">
-      <div class="">
-        <div>
-          <h3>Login</h3>
-          <hr />
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card border-0 shadow rounded-3 my-5">
+          <div class="card-body p-4 p-sm-5 mb-5">
+            <h3 class="text-center">Login</h3>
+             <hr />
+            <form>
+              <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="loginEmail" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+              </div>
+              <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="loginPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+              </div>
+
+              <div class="form-check mb-3">
+                <input class="form-check-input" type="checkbox" value="" id="rememberPasswordCheck">
+                <label class="form-check-label" for="rememberPasswordCheck">
+                  Remember my password
+                </label>
+              </div>
+              <div class="d-grid">
+                <button class="btn btn-primary" type="submit">Login</button>
+              </div>
+              <hr class="my-4">
+            </form>
+          </div>
         </div>
-        <form>
-          <div class="form-group">
-            <label> Email </label>
-            <input type="text" class="form-control" />
-          </div>
-          <div class="form-group">
-            <label> Password </label>
-            <input type="password" class="form-control" />
-          </div>
-          <div class="my-3">
-            <button type="submit" class="btn btn-primary">Login</button>
-          </div>
-        </form>
       </div>
-    </div>
-  </div>
 </template>
