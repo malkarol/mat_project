249a3982ff7f4bfe8585f1f88f484ba208abbe10
lemiecm <mjlemiec@gmail.com>
2021-12-05 23:10:24 +0100

    Add utils file creation for FL

--- script_generator/general.py
+++ script_generator/general.py
@@ -3,6 +3,7 @@ import pickle
 
 user = "Michal"
 localPath = "./script_generator/generated_scripts/dummy/"
+realPath = "./script_generator/generated_scripts/pseudoReal/"
 
 def get_date_for_name():
     timeFormat = "%H%M%S%d%m%Y"
@@ -14,9 +15,16 @@ def get_text_from_file(fileName):
         lines = f.readlines()
     return lines
 
-def create_filename(extension=".py"):
+def create_filename(isPseudoReal=False,extension=".py"):
     fileName = get_date_for_name()
-    fileName = localPath+fileName+"_"+user+".py"
+    filePath = realPath if isPseudoReal else localPath
+    fileName = filePath+fileName+"_"+user+extension
+    return fileName
+
+def create_filename(name,isPseudoReal=False, extension=".py"):
+    fileName = get_date_for_name()
+    filePath = realPath if isPseudoReal else localPath
+    fileName = filePath+fileName+"_"+name+"_"+user+extension
     return fileName
 
 def create_dummy_python_file(fileName, sometext=""):
@@ -34,9 +42,6 @@ def script_to_textfile(inputFile,outputFile=""):
     with open(inputFile, "r") as _inputFile, open(outputFile, "w") as _outputFile:
         _outputFile.write(_inputFile.read())
 
-def create_common_part():
-    pass
-
 def script_to_pickle(inputFile,outputFile=""):
     if outputFile == "":
         outputFile = inputFile[:-2]+"pickles"
@@ -45,18 +50,65 @@ def script_to_pickle(inputFile,outputFile=""):
     with open(outputFile, "wb") as _outputFile:
         pickle.dump(readText, _outputFile)
 
+def write_chunk_to_file(destinationFile, chunkFile, sourceFile=""):
+    if sourceFile == "":
+        sourceFile = destinationFile
+    destinationText = get_text_from_file(sourceFile)+ get_text_from_file(chunkFile)
+    with open(destinationFile, 'w') as f:
+            for element in destinationText:
+                f.write(element)
 
-def get_parameters():
+
+def create_utils_MNIST():
+    fileName = create_filename("utils",True,".txt")
+    pathToFiles= "./script_generator/source_chunks/MNIST_chunks/core_chunks/declarations_and_functions/"
+    source_1 = pathToFiles+ "MNIST_importPackages.txt"
+    source_2 = pathToFiles+ "MNIST_createClients.txt"
+    source_3 = pathToFiles + "MNIST_loadImageData.txt"
+    source_4 = pathToFiles + "MNIST_batchData.txt"
+
+    write_chunk_to_file(fileName,source_2,source_1)
+    write_chunk_to_file(fileName,source_3)
+    write_chunk_to_file(fileName,source_4)
+
+    pathToFiles ="./script_generator/source_chunks/MNIST_chunks/model_dependant_chunks/"
+    source_5 = pathToFiles+"MNIST_test_model.txt"
+    source_6 = pathToFiles+"MNIST_simpleMLP_model.txt"
+    source_7 = pathToFiles +"MNIST_weighting_and_scaling.txt"
+
+    write_chunk_to_file(fileName,source_5)
+    write_chunk_to_file(fileName,source_6)
+    write_chunk_to_file(fileName,source_7)
+    pass
+
+def create_learning_with_fake_clients_MNIST():
+    pass
+
+def create_aggregator_script_MNIST():
+    pass
+
+def parametrize_chunk():
     """
-    :type listOfObjects: list<Object>
+    TO DO
+    :type params: list<string>
+    """
+    pass
+
+def get_parameters(params):
+    """
+    TO DO
+    :type params: list<string>
     """
     pass
 
 if __name__ == "__main__":
-    fileName = create_filename()
-    sometext = get_text_from_file("./script_generator/testSource.py")
-    create_dummy_python_file(fileName, sometext)
-    script_to_textfile(fileName)
-    script_to_pickle(fileName)
+    # fileName = create_filename()
+    # sometext = get_text_from_file("./script_generator/testSource.py")
+    # create_dummy_python_file(fileName, sometext)
+    # script_to_textfile(fileName)
+    # script_to_pickle(fileName)
+
+   create_utils_MNIST()
+
 
 
