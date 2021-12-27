dc905140efa3caea0ca8f156d46e775caedb6642
lemiecm <mjlemiec@gmail.com>
2021-12-27 14:02:33 +0100

    Add restrictions to see sessions and single session

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,12 +8,22 @@
 </template>
 
 <script>
+import axios from 'axios'
 import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
     NavBar
+  },
+  beforeCreate () {
+    this.$store.commit('initializeStore')
+
+    if (this.$store.state.token) {
+      axios.defaults.headers.common['Authorization'] = 'Token ' + this.$store.state.token
+    } else {
+      axios.defaults.headers.common['Authorization'] = ''
+    }
   }
 
 }
