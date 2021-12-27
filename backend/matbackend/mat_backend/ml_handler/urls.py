9e89c375c11482266f33a86d2da853d23303ad9e
lemiecm <mjlemiec@gmail.com>
2021-12-27 11:36:30 +0100

    Add rest of the endpoints

--- backend/matbackend/mat_backend/ml_handler/urls.py
+++ backend/matbackend/mat_backend/ml_handler/urls.py
@@ -4,5 +4,6 @@ from ml_handler import views
 
 urlpatterns=[
     path('mlmodels/',views.ml_models_list),
+    path('mlmodel/<int:pk>',views.ml_model_detail)
     # path('user/<int:pk>',views.user_details)
 ]
