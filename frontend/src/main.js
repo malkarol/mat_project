ba7b2cb80a0cf78e9c3f760b78c2e03f6885904b
lemiecm <mjlemiec@gmail.com>
2021-12-27 13:24:38 +0100

    Add vuex and store

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
-import axios from 'axios'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000/'
 
 const app = createApp(App)
+app.use(store)
 app.use(router, axios)
 app.mount('#app')
