06a7632b0f1c302cb38fb5d9104a218559b5f50c
lemiecm <mjlemiec@gmail.com>
2022-01-01 23:09:02 +0100

    Create CreateNewSession view

---
+++ frontend/src/views/CreateNewSession.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="col-sm-9 col-md-7 col-lg-8 mx-auto">
+        <div class="card border-0 shadow rounded-3 my-5">
+          <div class="card-body p-4 p-sm-5 mb-5">
+        <div>
+          <h3>Registration</h3>
+          <hr />
+        </div>
+         <div class="row">
+      <div class="d-flex justify-content-between">
+        <div class="col">
+          <h2>Tymczasowa nazwa sesji</h2>
+          <div class="d-flex flex-row pt-2 border-bottom">
+            <div class="p-2">
+              <p class="mb-1"><strong>Owner:</strong></p>
+            </div>
+            <div class="p-2">
+              <p class="mb-1">Karol Malinowski</p>
+            </div>
+          </div>
+        </div>
+        <div class="border-bottom align-self-end">
+          <p class="text-primary"><strong>IN PROGRESS</strong></p>
+        </div>
+      </div>
+    </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+<script>
+
+
+export default {
+  data () {
+
+  },
+  methods: {
+
+    },
+}
+</script>
