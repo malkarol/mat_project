e5b2a95b62b83c9d36886568690a3c0a3828bfa2
lemiecm <mjlemiec@gmail.com>
2022-01-01 23:09:50 +0100

    Add Footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+      <div>Project logo made by <a href="https://www.flaticon.com/authors/konkapp" title="Konkapp">Konkapp</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">Michał Lemiec</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+    </div>
+</template>
