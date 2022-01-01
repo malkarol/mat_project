f2e5a3994bb670de9925725ad41526aac1bb24d4
lemiecm <mjlemiec@gmail.com>
2022-01-01 19:57:16 +0100

    Update NavBar according to main branch

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -13,10 +13,10 @@
         <li class="nav-item">
           <router-link class="nav-link" to="/">Getting started</router-link>
         </li>
-        <li class="nav-item">
+        <li v-if="isLogged() === true"  class="nav-item">
           <router-link class="nav-link" to="/session">SingleSession[Preview]</router-link>
         </li>
-        <li class="nav-item">
+        <li v-if="isLogged() === true"  class="nav-item">
           <router-link class="nav-link" to="/sessions">Sessions</router-link>
         </li>
 
