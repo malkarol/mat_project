ec9ec4ea3d24131236d419748eefdfc31eb1993f
lemiecm <mjlemiec@gmail.com>
2022-01-14 12:35:36 +0100

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -9,7 +9,7 @@
                 MAT - Model Aggregation Tool
               </h1>
               <p class="lead text-white-50 mb-4">
-                A web platform for collaborative machine learning without centralized training data 
+                A web platform for collaborative machine learning without centralized training data
               </p>
               <div class="d-grid gap-3 d-sm-flex justify-content-sm-center">
                 <a class="btn btn-primary btn-lg px-4 me-sm-3" href="#features"
@@ -19,7 +19,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="24" fill="currentColor" class="bi bi-github" viewBox="0 2 16 16">
                     <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
                     </svg>
-                    Documentation</a
+                    GitHub</a
                 >
               </div>
             </div>
@@ -44,19 +44,13 @@
                         <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i></div>
                         <h2 class="h4 fw-bolder">Free to use</h2>
                         <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                        <a class="text-decoration-none" href="#!">
-                            Call to action
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
+
                     </div>
                     <div class="col-lg-4">
                         <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
                         <h2 class="h4 fw-bolder">No data sharing</h2>
                         <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                        <a class="text-decoration-none" href="#!">
-                            Call to action
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
+
                     </div>
                 </div>
             </div>
@@ -95,11 +89,11 @@
                                         <i class="bi bi-check text-primary"></i>
                                         More user interaction required
                                     </li>
-                                    <li class="mb-2 text-muted">
+                                    <li class="mb-2">
                                         <i class="bi bi-x"></i>
                                         Less automated learning
                                     </li>
-                                    <li class="mb-2 text-muted">
+                                    <li class="mb-2 ">
                                         <i class="bi bi-x"></i>
                                         Sessions access restriction
                                     </li>
