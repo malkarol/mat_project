2d98dfe37cae9a994414ed20a1007cee5ddddace
lemiecm <mjlemiec@gmail.com>
2022-01-19 12:19:17 +0100

    Add getting started

--- frontend/src/views/GettingStarted.vue
+++ frontend/src/views/GettingStarted.vue
@@ -10,6 +10,129 @@
                 <div>
                     <h4>Overview</h4>
                 </div>
+                <p>
+                    The following document allows you to understand how our software works and how to use.
+                    <br> Despite our best efforts, please be patient during running Machine Learning scripts
+                    <br>and remeber that some error and issues might be not handled.
+                </p>
+                <hr>
+                <div>
+                    <h4>Prerequisites</h4>
+                </div>
+                <p>In order to use <strong>MAT</strong> webpage you don't have to have anything installed.
+                <br>However, in order to use Python scripts on your PC it is adviced that you have <strong>Python 3.7+</strong> and <strong>Tensorflow 2</strong> installed.
+                <br>If you don't have them please go to the following web pages, download them and use tutorials for installation.
+                    </p>
+                    <ul>
+                        <li><a href="https://www.python.org/downloads/">Python</a> </li>
+                        <li><a href="https://www.tensorflow.org/install">Tensorflow</a></li>
+
+                    </ul>
+
+
+                <hr/>
+
+                <div>
+                    <h4>Register</h4>
+                </div>
+                <p>
+                    Before you even start thinking about experiencing Federated Learning, please create new account.
+                    <br> In order to do so, click <strong>"Register"</strong> at the right corner of blue navbar.
+                    <br> Then you will be given 6 inputs to fill.
+                    <br> Fill them with the appropiate values (e.g not common password, password no less than 8 characters).
+                    <br> If any issue will occur it is going to come up at the top of the page.
+                </p>
+                <hr/>
+
+                <div>
+                    <h4>Log in</h4>
+                </div>
+                <p>
+                After registration or if you haven't done it already please login using previously created credentials.
+
+                </p>
+                <hr/>
+
+                <div>
+                    <h4>Create first session</h4>
+                </div>
+                <p>
+                    In order to create our first session click on the <strong>"Create new session"</strong> button
+                </p>
+                <hr/>
+
+                <div>
+                    <h4>Sessions panel</h4>
+                </div>
+                <p>
+                    On our <strong> Sessions </strong> panel user is presented with all sessions created by anyone in our system.
+                    <br> As you might observe we have two different <strong>paying plans</strong>.
+                    <br> The general difference is the fact that <strong> Premium </strong> plan comes on-server model aggregation
+                    <br> and allows users to create <strong>Private</strong> sessions.
+                    <br> That is why, depeneding on the plan you chose different filter panel you will get.
+                    <br> For the both of plans you have the following filters:
+                    <ul>
+                        <li>
+                            Filter by session name
+                        </li>
+                         <li>
+                            Filter by session tag
+                        </li>
+                        <li>
+                            Show <strong>Created sessions</strong> checkbox filter
+                        </li>
+                         <li>
+                            Show <strong>Joined sessions</strong> checkbox filter
+                            <br> <span class="text-muted"> Notice that when you create session you also join session</span>
+                        </li>
+                    </ul>
+                    <br> For <strong> Premium </strong> plan you can additionally filter by:
+                    <ul>
+                        <li>
+                            <strong>Public sessions</strong> checkbox filter
+                        </li>
+                        <li>
+                            <strong>Private sessions</strong> checkbox filter
+                        </li>
+                    </ul>
+                    <br>
+                    <br>
+                    <br> If you just created your new session for the first time click checkbox name <strong>"Created sessions"</strong>.
+                    <br> Then the only session you will see will be the one that you previously created.
+                    <br>Click on newly created session card, then click <strong>"Go to session"</strong> button.
+
+                </p>
+                <hr/>
+
+                <div>
+                    <h4>Session panel</h4>
+                </div>
+                <p>
+                    After going into session you will see session panel that is only presented to participants of the given session.
+                    <br>
+                    <br>
+                </p>
+                <ul>
+                    <li>
+                        <h5> Local learning </h5>
+                        <p>
+                            In order to do local learning,
+                        </p>
+                    </li>
+                    <li>
+                        <h5> Aggregation </h5>
+                        <p>
+                            In order to do local learning,
+                        </p>
+                    </li>
+                    <li>
+                        <h5> Results </h5>
+                        <p>
+                            In order to do local learning,
+                        </p>
+                    </li>
+                </ul>
+
             </section>
         </div>
 
