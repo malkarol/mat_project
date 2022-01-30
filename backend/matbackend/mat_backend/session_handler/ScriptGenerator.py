d956b5a5f88ec2696a47246155382f0d01f60002
lemiecm <mjlemiec@gmail.com>
2022-01-30 01:07:03 +0100

    Refactor ScriptGenerator

--- backend/matbackend/mat_backend/session_handler/ScriptGenerator.py
+++ backend/matbackend/mat_backend/session_handler/ScriptGenerator.py
@@ -8,13 +8,9 @@ storage = GoogleCloudStorage()
 from abc import ABC, abstractmethod
 from typing import Any, Optional
 
+# source: https://refactoring.guru/design-patterns/chain-of-responsibility/python/example
 
 class Handler(ABC):
-    """
-    The Handler interface declares a method for building the chain of handlers.
-    It also declares a method for executing a request.
-    """
-
     @abstractmethod
     def set_next(self, handler: "Handler") -> "Handler":
         pass
@@ -25,18 +21,10 @@ class Handler(ABC):
 
 
 class AbstractHandler(Handler):
-    """
-    The default chaining behavior can be implemented inside a base handler
-    class.
-    """
-
     _next_handler: Handler = None
 
     def set_next(self, handler: Handler) -> Handler:
         self._next_handler = handler
-        # Returning a handler from here will let us link handlers in a
-        # convenient way like this:
-        # monkey.set_next(squirrel).set_next(dog)
         return handler
 
     @abstractmethod
@@ -45,12 +33,6 @@ class AbstractHandler(Handler):
             return self._next_handler.handle(request, parameters)
 
         return None
-
-
-"""
-All Concrete Handlers either handle a request or pass it to the next handler in
-the chain.
-"""
 
 class ParametersScript(AbstractHandler):
     def handle(self, request: Any, parameters) -> str:
@@ -153,29 +135,16 @@ class AggregateLocallyScript(AbstractHandler):
 
 
 def generate_local_model(handler: Handler, session_params) -> None:
-    """
-    The client code is usually suited to work with a single handler. In most
-    cases, it is not even aware that the handler is part of a chain.
-    """
     commands = ['preparation','model','parameters','load','local_learning']
     results = []
     for comm in  commands:
-        print(f"\nClient: Who wants a {comm}?")
         result = handler.handle(comm,session_params)
         if result:
-            print(f"  {result}", end="")
             results.append(result)
-        else:
-            print(f"  {comm} was left untouched.", end="")
-
     return results
 
 
 def generate_initial_weights(handler: Handler, session_params) -> None:
-    """
-    The client code is usually suited to work with a single handler. In most
-    cases, it is not even aware that the handler is part of a chain.
-    """
     commands = ['preparation','model','parameters','initialize_weights']
     results = []
     for comm in  commands:
@@ -185,10 +154,6 @@ def generate_initial_weights(handler: Handler, session_params) -> None:
     return results
 
 def generate_aggregation_script(handler: Handler, session_params) -> None:
-    """
-    The client code is usually suited to work with a single handler. In most
-    cases, it is not even aware that the handler is part of a chain.
-    """
     commands = ['preparation','model','parameters','aggregate_locally']
     results = []
     for comm in  commands:
@@ -198,9 +163,6 @@ def generate_aggregation_script(handler: Handler, session_params) -> None:
     return results
 
 def generate_global_model(handler: Handler, session_params) -> None:
-    """
-    The template method defines the skeleton of an algorithm.
-    """
     commands = ['preparation','model','pre','parameters','load_global','local_learning']
     results = []
     for comm in  commands:
@@ -210,6 +172,7 @@ def generate_global_model(handler: Handler, session_params) -> None:
     return results
 
 class ScriptsExecutor():
+
     @staticmethod
     def create_local_model(parameters):
         params_script = ParametersScript()
