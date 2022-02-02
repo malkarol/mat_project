46ab3f13c4a61c58dceee104f0783b987bd3f326
lemiecm <mjlemiec@gmail.com>
2022-02-02 20:56:03 +0100

    Update moving_files.py

--- ml_files_templates/moving_files.py
+++ ml_files_templates/moving_files.py
@@ -2,25 +2,84 @@ import shutil
 import os
 import random
 
-source = '/Users/michal/Downloads/CIFAR-10-images-master/test/'
+source = '/Users/michal/Desktop/covid/'
 count = 1
 
 directories = os.listdir(source)
 
-while count!=100:
+
+# moving to test
+while count != 1500:
     dir = random.choice(directories)
-    while dir ==".DS_Store":
+    while dir ==".DS_Store" or len(os.listdir(source+dir+"/"))==0:
         dir = random.choice(directories)
     files = os.listdir(source+dir+"/")
     file = random.choice(files)
-    target ="/Users/michal/Desktop/cifar10/test/"+dir+"/"
-    if not os.path.exists("/Users/michal/Desktop/cifar10/test/"+dir+"/"):
-        os.makedirs("/Users/michal/Desktop/cifar10/test/"+dir+"/")
+    target =f"/Users/michal/Desktop/covid 2.02/test/"+dir+"/"
+    if not os.path.exists(target):
+        os.makedirs(target)
     shutil.move(source+dir+"/"+file, target)
-    count=count+1
+    count += 1
+
+# # moving to datasets
+for i in range(1,4):
+    for j in range(1, 6):
+        count = 0
+        while count != 200:
+            dir = random.choice(directories)
+            while dir ==".DS_Store" or len(os.listdir(source+dir+"/"))==0:
+                dir = random.choice(directories)
+            files = os.listdir(source+dir+"/")
+            file = random.choice(files)
+            target =f"/Users/michal/Desktop/covid 2.02/dataset{i}/part{j}/"+dir+"/"
+            if not os.path.exists(target):
+                os.makedirs(target)
+            shutil.move(source+dir+"/"+file, target)
+            count += 1
+
+# moving to datasets unbalanced
+for i in range(1,4):
+    for j in range(1, 6):
+        count = 0
+        while count != 200:
+            if i == 1:
+                dir = 'Normal'
+            elif i == 2:
+                dir = 'COVID'
+            else:
+                dir = 'Lung_Opacity'
+            while dir ==".DS_Store" or len(os.listdir(source+dir+"/"))==0:
+               break
+            files = os.listdir(source+dir+"/")
+            print(len(files))
+            file = random.choice(files)
+            target =f"/Users/michal/Desktop/covid 2.02/dataset{i}/part{j}/"+dir+"/"
+            if not os.path.exists(target):
+                os.makedirs(target)
+            shutil.move(source+dir+"/"+file, target)
+            count += 1
+
 
 # count = 0
-# for f in files:
-#     if (count<300):
-#         shutil.move(f, dest1)
-print(directories)
+# while count != 184:
+#     source=f"/Users/michal/Desktop/covid 2.02/additional/"
+#     dir = "COVID"
+#     while dir ==".DS_Store" or len(os.listdir(source+dir+"/"))==0:
+#         break
+#     if len(os.listdir(source+dir+"/"))==0:
+#         print(f'Count: {count}')
+#         break
+#     files = os.listdir(source+dir+"/")
+#     file = random.choice(files)
+#     target =f"/Users/michal/Desktop/covid 2.02/dataset2/part5/"+dir+"/"
+#     if not os.path.exists(target):
+#         os.makedirs(target)
+#     shutil.move(source+dir+"/"+file, target)
+#     count += 1
+# #
+for i in range(1,4):
+    for j in range(1, 6):
+        target =f"/Users/michal/Desktop/covid 2.02/dataset{i}/part{j}/"
+        length = sum([len(files) for r, d, files in os.walk(f"/Users/michal/Desktop/covid 2.02/dataset{i}/part{j}/")])
+        print(f'num of files for dataset{i}, part{j}: {length}')
+
